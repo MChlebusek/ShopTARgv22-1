6a948a78160df49ee6fd13ce901d343df9b0d99a
Ingvar Derešivski <inde999@gmail.com>
2023-09-11 12:06:28 +0300

    SpaceshipIndexComplete

--- Shop/Shop/Controllers/SpaceshipsController.cs
+++ Shop/Shop/Controllers/SpaceshipsController.cs
@@ -1,13 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
+using Shop.Models;
+using Shop.Models.Spaceship;
 
 namespace Shop.Controllers
 {
     public class SpaceshipsController : Controller
     {
+        private readonly ShopContext _context;
+
+        public SpaceshipsController
+            (
+                ShopContext context
+            )
+        {
+            _context = context;
+        }
+
 
         public IActionResult Index()
         {
-            return View();
+            var result = _context.Spaceships
+                .Select(x => new SpaceshipIndexViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Type = x.Type,
+                    EnginePower = x.EnginePower,
+                    Passengers = x.Passengers
+                });
+
+            return View(result);
         }
 
         [HttpGet]
