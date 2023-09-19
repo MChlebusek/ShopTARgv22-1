5421b3a25858435b4702b4c6624ed5fc446d796b
Ingvar Derešivski <inde999@gmail.com>
2023-09-19 15:53:28 +0300

    CRUDFixesAndFileToApiInProgress

--- Shop/Shop.Core/Dto/SpaceshipDto.cs
+++ Shop/Shop.Core/Dto/SpaceshipDto.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
+
 
 namespace Shop.Core.Dto
 {
@@ -20,5 +17,10 @@ namespace Shop.Core.Dto
         //only in database
         public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
+
+        public List<IFormFile> Files { get; set; }
+
+        public IEnumerable<FileToApiDto> FileToApiDtos { get; set; }
+            = new List<FileToApiDto>();
     }
 }
